45deef39123dea3b2a6a20e4160a6506edfdb22b
Hubert <Hubert_XH@163.com>
2020-05-31 21:35:57 +0800

    init project

---
+++ src/test/java/cn/buaa/Sagittarius/SagittariusApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.buaa.Sagittarius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagittariusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
